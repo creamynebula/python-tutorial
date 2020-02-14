402254b014ead05fcc9cc3d1f84636b4b4abceea
Mateus <mateus.souzaaa@gmail.com>
2020-02-14 15:02:21 -0300

    como truncar floats

--- yolo4.py
+++ yolo4.py
@@ -1,7 +1,20 @@
+import math as math
+
+
 def mod5_x(x):
     return x % 5
 
 
 vect1 = [100, 51, 14]
-print('Que número é o maior em mod 5,', vect1, '?', max(vect1, key=mod5_x))
+print('\nQue número é o maior em (mod 5),', vect1, '?', max(vect1, key=mod5_x))
 print('\n', help(max))
+
+
+def truncate(number, n):
+    return math.floor(number*10**n)/10**n
+
+
+pi = 3.14159265359
+x = truncate(pi, 5)
+
+print('\nPi, 5 dígitos depois da vírgula:', x)
