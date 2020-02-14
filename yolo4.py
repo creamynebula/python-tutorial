ba119d3fc0fd4b126a5e69ffdb43c963651ca0f6
Mateus <mateus.souzaaa@gmail.com>
2020-02-14 15:58:46 -0300

    if/else em 1 linha

--- yolo4.py
+++ yolo4.py
@@ -18,3 +18,12 @@ pi = 3.14159265359
 x = truncate(pi, 5)
 
 print('\nPi, 5 dígitos depois da vírgula:', x)
+
+
+def quiz_message(grade):
+    outcome = 'deu ruim' if grade < 60 else 'passou'
+    print('\nSua situação final:', grade, outcome)
+
+
+quiz_message(59)
+quiz_message(61)
