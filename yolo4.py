26cfefbc27a46735479cb193eec6f6c85beeaf3d
Mateus <mateus.souzaaa@gmail.com>
2020-02-16 09:07:45 -0300

    insert, append, extend, remove, pop, index

--- yolo4.py
+++ yolo4.py
@@ -29,13 +29,24 @@ quiz_message(59)
 quiz_message(61)
 
 lista1, lista2 = [1, 2, 4, 9, 6, 10, 11, 12], [1, 1, 1, 1]
+print('lista 1 no começo:', lista1)
+print('lista 2 no começo:', lista2)
 lista1.append(13)
 lista1.extend(lista2)
+print('lista 1 dps de add "13" e os elementos de lista 2:', lista1)
+
 # insert(i, elemento) insere elemento ANTES da posiçao i
 lista1.insert(len(lista1)-1, 'penultimo')
 print(lista1)
+
 # remove o 1o elemento que tem esse valor, se nao tiver nenhum retorna ValueError
 lista1.remove('penultimo')
+
 # lista1.pop(i) remove o elemento em i, ou remove o último se nao especificar índice
 lista1.pop(len(lista1)-1)
 print(lista1)
+
+# lista1.clear() removeria todos os elementos da lista
+
+print('Quantidade de 1\'s:', lista1.count(1))
+print('Index do elemento 6, se houver:', lista1.index(6))
