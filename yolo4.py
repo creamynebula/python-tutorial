3989bbc2f6b75ebbb299cebfe4f28feb67365518
Mateus <mateus.souzaaa@gmail.com>
2020-02-16 07:54:34 -0300

    operacoes em listas

--- yolo4.py
+++ yolo4.py
@@ -22,8 +22,20 @@ print('\nPi, 5 dígitos depois da vírgula:', x)
 
 def quiz_message(grade):
     outcome = 'deu ruim' if grade < 60 else 'passou'
-    print('\nSua situação final:', grade, outcome)
+    print('\nSua situação final:', grade, outcome, '\n')
 
 
 quiz_message(59)
 quiz_message(61)
+
+lista1, lista2 = [1, 2, 4, 9, 6, 10, 11, 12], [1, 1, 1, 1]
+lista1.append(13)
+lista1.extend(lista2)
+# insert(i, elemento) insere elemento ANTES da posiçao i
+lista1.insert(len(lista1)-1, 'penultimo')
+print(lista1)
+# remove o 1o elemento que tem esse valor, se nao tiver nenhum retorna ValueError
+lista1.remove('penultimo')
+# lista1.pop(i) remove o elemento em i, ou remove o último se nao especificar índice
+lista1.pop(len(lista1)-1)
+print(lista1)
