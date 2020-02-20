a96a10a59807c20e5d41bda08d387cf3646149e5
Mateus <mateus.souzaaa@gmail.com>
2020-02-20 19:59:19 -0300

    enumerate(lista) retorna lista de tuplas (indice, item)

---
+++ yolo5.py
@@ -0,0 +1,22 @@
+animals = ['zebra', 'guaxinim', 'lontra', 'dragão']
+chars = 0
+for x in animals:
+    chars += len(x)
+print('Número de caracteres: {}    Tamanho médio: {}'.format(
+    chars, chars/len(animals)))
+
+
+# recebe [a0,a1,a2...,an-1] e retorna [a0,a2,a4,...]
+def skip_elements(elements):
+    enumerados = enumerate(elements)
+    resultado = []
+    for index, item in enumerados:
+        if index % 2 == 0:
+            resultado.append(item)
+    return resultado
+
+
+# Should be ['a', 'c', 'e', 'g']
+print(skip_elements(["a", "b", "c", "d", "e", "f", "g"]))
+# Should be ['Orange', 'Strawberry', 'Peach']
+print(skip_elements(['Orange', 'Pineapple', 'Strawberry', 'Kiwi', 'Peach']))
