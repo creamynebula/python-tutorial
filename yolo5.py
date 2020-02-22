0a8af37ce7501f29ac7b0d3dd84071df6e85c374
Mateus <mateus.souzaaa@gmail.com>
2020-02-22 20:25:45 -0300

    new mario kart item, fashionably late at parties

--- yolo5.py
+++ yolo5.py
@@ -39,3 +39,36 @@ b = 0
 # em python podemos a,b = b,a pra inverter os valores, porque ele trata isso como (a,b) = (b,a)
 a, b = b, a
 print(a, b)
+
+
+def select_second(L):
+    """Return the second element of the given list. If the list has no second
+    element, return None.
+    """
+    if len(L) > 1:
+        return L[1]
+    return None
+
+
+def purple_shell(racers):
+    """Given a list of racers, set the first place racer (at the front of the list) to last
+    place and vice versa.
+
+    >>> r = ["Mario", "Bowser", "Luigi"]
+    >>> purple_shell(r)
+    >>> r
+    ["Luigi", "Bowser", "Mario"]
+    """
+    racers[0], racers[len(racers)-1] = racers[len(racers)-1], racers[0]
+
+
+def fashionably_late(arrivals, name):
+    """Given an ordered list of arrivals to the party and a name, return whether the guest with that
+    name was fashionably late.
+    """
+    fashionable = len(arrivals) / 2
+    last = len(arrivals) - 1
+    when = arrivals.index(name)
+    if when >= fashionable and when < last:
+        return True
+    return False
